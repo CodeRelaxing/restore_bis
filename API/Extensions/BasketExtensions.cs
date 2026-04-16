f033960098181006f9d96e813537bccab6df7bd8
Codeandrelax <codeandrelax@gmail.com>
2026-04-16 22:35:18 +0200

    End of section 10

--- API/Extensions/BasketExtensions.cs
+++ API/Extensions/BasketExtensions.cs
@@ -1,6 +1,8 @@
 using System;
 using API.DTOs;
 using API.Entities;
+using Microsoft.Build.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
 
@@ -11,6 +13,8 @@ public static class BasketExtensions
         return new BasketDto
         {
             BasketId = basket.BasketId,
+            ClientSecret = basket.ClientSecret,
+            PaymentIntentId = basket.PaymentIntentId,
             Items = basket.Items.Select(x => new BasketItemDto
             {
                 ProductId = x.ProductId,
@@ -22,5 +26,13 @@ public static class BasketExtensions
                 Quantity = x.Quantity
             }).ToList()
         };
+    }
+
+    public static async Task<Basket> GetBasketWithItems(this IQueryable<Basket> query, string? basketId)
+    {
+        return await query
+            .Include(x => x.Items)
+            .ThenInclude(x => x.Product)
+            .FirstOrDefaultAsync(x => x.BasketId == basketId) ?? throw new Exception("Cannot get basket");
     }
 }
